991d2631416122a5babedec57fbede92259b9789
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2024-06-02 18:48:59 -0600

    Add Prisma generation script and body-parser dependency

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { config } from 'dotenv'
 
 import usersRouter from './routes/contacts.route'
 import { resolve } from 'path'
+import bodyParser from 'body-parser'
 
 /* Read environment variables */
 config()
@@ -15,6 +16,7 @@ const port = process.env.PORT ?? 3000
 app.use(express.static(resolve(__dirname, 'public')))
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
